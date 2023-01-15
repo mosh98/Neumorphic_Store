8c5123b7c973e4d9c86732558e89067275a6f222
moslehmahamud <mosleh.edu@gmail.com>
2023-01-15 08:05:37 +0100

    work buttion fixed

--- laptopX2.js
+++ laptopX2.js
@@ -2,10 +2,16 @@ var select = document.getElementById("dropdown");
 var productFeatures = document.getElementById("product-features")
 var productDescription = document.getElementById("Description")
 var secondTitle = document.getElementById("second-card-title")
-
+var workBalance = document.getElementById("work-balance")
 var image = document.getElementById("image")
 
+var workButton = document.getElementById("work-btn")
+var bankButton = document.getElementById("bank-btn")
+
+
+
 let laptops = []
+let workBalanceVariable = 0
 
 fetch('https://hickory-quilled-actress.glitch.me/computers')
     .then((response) => response.json() )
@@ -38,6 +44,7 @@ const populateLaptopInfo = (laptopInstance) => {
     //Price
 }
 
+
 const handleFeature = e => {
     /*
     * write comments: what is this function doing?
@@ -46,23 +53,29 @@ const handleFeature = e => {
 
     const selectedComp = laptops.find(x => x.id == e.target.value) //be carefull of this line
 
-    productFeatures.innerText = selectedComp.specs
-    //Product Description
-    productDescription.innerText = selectedComp.description
-    //Second title
-    secondTitle.textContent = selectedComp.title
-    // set image
-    image.src = 'https://hickory-quilled-actress.glitch.me/computers'.replace('computers','') + selectedComp.image
-
+    populateLaptopInfo(selectedComp)
     //Price
 
 }
 
+
 select.addEventListener("change", handleFeature)
 
 //add event lister for Get a Loan
+
 //add event lister for WORK
-    //add event listerne for bank button
+
+workButton.addEventListener("click", function (){
+        var text = workBalance.textContent
+        var number = Number(text);
+
+
+        console.log(number)
+        workBalance.innerText = number + 100
+        workBalanceVariable = number + 100
+
+})
+//add event listerne for bank button
         //move balance
 
 
